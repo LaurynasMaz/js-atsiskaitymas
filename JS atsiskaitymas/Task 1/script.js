3eac84ba48ae56441cf31cad201693fae2922045
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-01-02 09:28:39 +0200

    task 1 done

---
+++ JS atsiskaitymas/Task 1/script.js
@@ -0,0 +1,24 @@
+/* ------------------------------ TASK 1 ----------------------------
+Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
+pamatyti jo pateikto svorio konvertavimą į:
+1. Svarus (lb) | Formulė: lb = kg * 2.2046
+2. Gramus (g) | Formulė: g = kg / 0.0010000
+3. Uncijos (oz) | Formulė: oz = kg * 35.274
+
+Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
+<div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
+------------------------------------------------------------------- */
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const kg = document.querySelector('#search').value;
+  const lb = kg * 2.2046;
+  const g = kg / 0.0010000;
+  const oz = kg * 35.274;
+
+  const output = document.querySelector('#output');
+  output.innerHTML = `
+      <h3>${lb} svarai</h3>
+      <h3>${g} gramai</h3>
+      <h3>${oz} uncijos</h3>
+  `;
+});
