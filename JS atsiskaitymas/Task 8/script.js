a2c04cf0661f65c4bab72aa7ae7e0fc95547534f
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-01-02 10:57:33 +0200

    task 8 done

--- JS atsiskaitymas/Task 8/script.js
+++ JS atsiskaitymas/Task 8/script.js
@@ -5,3 +5,32 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+  constructor(number1, number2){
+    this.number1 = number1
+    this.number2 = number2
+  }
+  sum() {
+    return this.number1 + this.number2;
+  }
+
+  subtraction() {
+    return this.number1 - this.number2;
+  }
+
+  multiplication() {
+    return this.number1 * this.number2;
+  }
+
+  division() {
+    return this.number1 / this.number2;
+  }
+}
+
+let calculator = new Calculator(5,7)
+
+console.log(calculator.sum())
+console.log(calculator.subtraction())
+console.log(calculator.multiplication())
+console.log(calculator.division())
